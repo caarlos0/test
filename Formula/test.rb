777c0d0f3a05ee22c140af1e18c8b3f7d24fc3b6
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2019-01-18 13:46:58 -0200

    Update test.rb

--- Formula/test.rb
+++ Formula/test.rb
@@ -1,9 +1,16 @@
 class Test < Formula
   desc ""
   homepage ""
-  url "https://github.com/caarlos0/test/releases/download/5.3.0/test_5.3.0_darwin_amd64.tar.gz"
+
+  if OS.mac?
+    url "https://github.com/caarlos0/test/releases/download/5.3.0/test_5.3.0_darwin_amd64.tar.gz"
+    sha256 "c08c267a92e5ff07089903dc821fb8f7c7428bb852f58531ed441c8407faa1aa"
+  else
+    url "https://github.com/caarlos0/test/releases/download/5.3.0/test_5.3.0_linux_amd64.tar.gz"
+    sha256 "whatever"
+  end
+
   version "5.3.0"
-  sha256 "c08c267a92e5ff07089903dc821fb8f7c7428bb852f58531ed441c8407faa1aa"
 
   def install
     bin.install "moises"
